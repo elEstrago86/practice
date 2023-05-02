12da9265ffa0d2e712d2faa3d4cf3e069ba1763b
elEstrago86 <124812151+elEstrago86@users.noreply.github.com>
2023-05-02 17:12:13 +0300

    Add files via upload

---
+++ cats.py
@@ -0,0 +1,9 @@
+from pets import Cat
+
+cat_1 = Cat("Барон", "Мальчик", "2 года")
+print(f'имя: {cat_1.name}, пол: {cat_1.gender}, возраст: {cat_1.age}')
+
+cat_2 = Cat("Сэм", "Мальчик", "2 года")
+print(f'имя: {cat_2.name}, пол: {cat_2.gender}, возраст: {cat_2.age}')
+
+
